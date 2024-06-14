d1d0987d2baa3a154e24bf07e2215346f8cb6ffe
PaulaSanchez <pausanchezt12@gmail.com>
2024-06-14 16:32:24 -0500

    feature: Home screen theme settings

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#4A2173",
+      },
+    },
+  },
+  plugins: [],
+};
