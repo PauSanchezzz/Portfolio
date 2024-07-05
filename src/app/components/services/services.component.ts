1bff40a8e7c43c26114b84cad0e1b66a6b6b6a8f
PaulaSanchez <pausanchezt12@gmail.com>
2024-07-05 12:56:01 -0500

    added services component

---
+++ src/app/components/services/services.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { ServiceCardComponent } from '../service-card/service-card.component';
+import { Title } from '@angular/platform-browser';
+import { title } from 'process';
+
+@Component({
+  selector: 'app-services',
+  standalone: true,
+  imports: [ServiceCardComponent],
+  templateUrl: './services.component.html',
+})
+export class ServicesComponent {
+  InformationCardService = [
+    {
+      title: 'Diseño Web UI/UX',
+      image: '/svg/design.svg',
+      description:
+        'Conocimiento en la creación de interfaces de usuario intuitivas y atractivas, siempre pensando en la mejor experiencia para el usuario.',
+    },
+    {
+      title: 'Desarrollo Frontend',
+      image: '/svg/frontend.svg',
+      description:
+        'Conocimiento en el desarrollo de interfaces web modernas y responsivas empleando tecnologías como Angular, HTML, CSS y JavaScript.',
+    },
+    {
+      title: 'Desarrollo Backend',
+      image: '/svg/backend.svg',
+      description:
+        'Experiencia con Laravel, Node.js y bases de datos tanto relacionales como no relacionales, garantizando sistemas robustos y eficientes.',
+    },
+    {
+      title: 'Desarrollo Android',
+      image: '/svg/mobile.svg',
+      description:
+        'Habilidades en desarrollo Android utilizando MVVM, Kotlin, Firebase, Jetpack Compose y XML para crear aplicaciones atractivas y funcionales.',
+    },
+    {
+      title: 'Análisis de datos',
+      image: '/svg/data.svg',
+      description:
+        'Habilidad en la gestión y análisis de datos utilizando bases de datos, Power BI y Excel, proporcionando insights valiosos para la toma de decisiones.',
+    },
+  ];
+}
