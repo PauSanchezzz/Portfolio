c6084070fbf28dfae0e3dcb9642e055c33af2fde
PaulaSanchez <pausanchezt12@gmail.com>
2024-07-08 18:20:15 -0500

    new projects added

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TechnologyProjectComponent } from '../technology-project/technology-project.component';
 import { ButtonProjectComponent } from '../button-project/button-project.component';
 import { CommonModule } from '@angular/common';
+import { link } from 'node:fs';
 
 @Component({
   selector: 'app-projects',
@@ -17,6 +18,8 @@ export class ProjectsComponent {
         'Cat Chat es una aplicación de chat exclusiva para amantes de los gatos. Aquí, los usuarios pueden registrarse con su correo electrónico, contraseña y nombre de usuario, y luego elegir entre cinco razas de gatos: Bombay, Tuxedo, Siames, Calico y Carey.',
       imageProject:
         'https://github.com/PauSanchezzz/CatChat/blob/main/src/client/public/cat3.png?raw=true',
+      github: 'https://github.com/PauSanchezzz/CatChat',
+      figma: '',
       technologies: [
         {
           background: 'bg-tecnology-blue',
@@ -44,5 +47,105 @@ export class ProjectsComponent {
         },
       ],
     },
+    {
+      titleProject: 'Dashboard - Animales con Microchip',
+      descriptionProject:
+        'Este dashboard, basado en un documento de Datos Abiertos de Bogotá, muestra la cifra de animales registrados hasta 2019 con microchips en diferentes zonas de la ciudad. Relaciona hembras y machos, especies (felinos y caninos), razas preferidas por estratos, y proporciona una lista de nombres, destacando los más frecuentes y los únicos.',
+      imageProject: 'dashboard.png',
+      github: 'https://github.com/PauSanchezzz/CatChat',
+      figma: '',
+      technologies: [
+        {
+          background: 'bg-[#4CAB7B]',
+          colorHover: 'hover:drop-shadow-darkgreen',
+          imgTechnology: '/svg/excel.svg',
+          title: 'Excel',
+        },
+        {
+          background: 'bg-yellow-600',
+          colorHover: 'hover:drop-shadow-yellow',
+          imgTechnology: '/svg/powerBi.svg',
+          title: 'Power BI',
+        },
+        {
+          background: 'bg-[#134D88]',
+          colorHover: 'hover:drop-shadow-tecnology-blue',
+          imgTechnology: '/svg/bd.svg',
+          title: 'DAX',
+        },
+      ],
+    },
+    {
+      titleProject: 'Portafolio Personal',
+      descriptionProject:
+        'Sitio web desarrollado para mostrar mis habilidades, proyectos y experiencias como desarrolladora. Creado con Angular 17+, HTML, CSS y Tailwind. Este portafolio incluye secciones dedicadas a mis proyectos destacados, habilidades técnicas, servicios y contacto. Además, cuenta con un diseño responsive y un menú superpuesto al contenido para una navegación intuitiva en dispositivos móviles.',
+      imageProject: 'portfolio.jpg',
+      github: 'https://github.com/PauSanchezzz/CatChat',
+      figma: '',
+      technologies: [
+        {
+          background: 'bg-background',
+          colorHover: 'hover:drop-shadow-purple',
+          imgTechnology: '/svg/angular.svg',
+          title: 'Angular',
+        },
+        {
+          background: 'bg-tecnology-blue',
+          colorHover: 'hover:drop-shadow-tecnology-blue',
+          imgTechnology: '/svg/tailwind.svg',
+          title: 'Tailwind CSS',
+        },
+        {
+          background: 'bg-orange-700',
+          colorHover: 'hover:drop-shadow-orange',
+          imgTechnology: '/svg/html.svg',
+          title: 'HTML',
+        },
+        {
+          background: 'bg-tecnology-blue',
+          colorHover: 'hover:drop-shadow-tecnology-blue',
+          imgTechnology: '/svg/css.svg',
+          title: 'CSS',
+        },
+      ],
+    },
+    {
+      titleProject: 'Life Chronicles',
+      descriptionProject:
+        'Aplicación móvil desarrollada con Kotlin y Jetpack Compose que ofrece una lista variada de lugares y planes en Bogotá. Permite explorar categorías de actividades con descripciones e imágenes, y cuenta con funciones de inicio de sesión, registro y perfil de usuario, utilizando Firebase. El proyecto fue desarrollado en colaboración con',
+      imageProject:
+        'https://github.com/PauSanchezzz/CatChat/blob/main/src/client/public/cat3.png?raw=true',
+      github: 'https://github.com/PauSanchezzz/CatChat',
+      figma: 'https://www.youtube.com/',
+      technologies: [
+        {
+          background: 'bg-background',
+          colorHover: 'hover:drop-shadow-purple',
+          imgTechnology: '/svg/kotlin.svg',
+          title: 'Kotlin',
+        },
+        {
+          background: 'bg-yellow-600',
+          colorHover: 'hover:drop-shadow-yellow',
+          imgTechnology: '/svg/firebase.svg',
+          title: 'Firebase',
+        },
+        {
+          background: 'bg-orange-700',
+          colorHover: 'hover:drop-shadow-orange',
+          imgTechnology: '/svg/figma.svg',
+          title: 'Figma',
+        },
+        {
+          background: 'bg-[#4CAB7B]',
+          colorHover: 'hover:drop-shadow-darkgreen',
+          imgTechnology: '/svg/jetpackCompose.svg',
+          title: 'Jetpack Compose',
+        },
+      ],
+    },
   ];
+  isSpecialName(name: string): boolean {
+    return name === 'Kotlin';
+  }
 }
