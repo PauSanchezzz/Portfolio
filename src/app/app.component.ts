cf7cf23edadbdd6623a3565b6b5373296fad9a9d
PaulaSanchez <pausanchezt12@gmail.com>
2024-08-22 18:00:36 -0500

    added animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -8,7 +8,8 @@ import { ServicesComponent } from './components/services/services.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { RolComponent } from './components/rol/rol.component';
 import { ContactComponent } from './components/contact/contact.component';
-
+// @ts-ignore
+import Typewriter from 't-writer.js';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -26,4 +27,23 @@ import { ContactComponent } from './components/contact/contact.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const target = document.querySelector('.tw');
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeColor: '#ECE1FF',
+      cursorColor: '#ECE1FF',
+    });
+
+    writer
+      .strings(
+        1700,
+        'Frontend Developer',
+        'Backend Developer',
+        'UX/UI Designer',
+        'Data Analyst',
+      )
+      .start();
+  }
+}
